f83f9271f963d45723fc86c99a1a7cf2959ce0f8
SiobhanP1 <siobhanphillips11@gmail.com>
2023-07-04 19:47:10 +0000

    Add loading image link to div under github id input field

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,7 +1,10 @@
-function fetchGithubInformation(event) {
+function fetchGitHubInformation(event) {
     var username = $("#gh-username").val();
     if (!username) {
-        $("gh-user-data").html(`<h2>Please enter a Github username.</h2>`);
+        $("#gh-user-data").html(`<h2>Please enter a Github username.</h2>`);
+        return;
     }
-    return;
+    // Add 'loading' image file
+    $("#gh-user-data").html(`<div id="loader">
+    <img src="assets/images/loader.gif" alt="loading..."></div>`);
 }
